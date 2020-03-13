ebbf5e52e32165bdd572f4ecc01b7a6c33e5cb5f
Velocityy1997 <you@example.com>
2020-03-13 16:12:39 +0800

    FirstModify

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">不像在湖南湿润</router-link> |
+      <router-link to="/about">不用涂都是嫩的</router-link>
     </div>
     <router-view/>
   </div>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>我想去南方</h1>
   </div>
 </template>
